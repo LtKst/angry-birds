6b7a2600e114b3298f9f243dde75143f3fce5d50
Yaruno292 <Yarunoyo@outlook.com>
2017-11-27 11:19:32 +0100

    Basic setup
    by me

--- Assets/Scripts/empty.cs
+++ Assets/Scripts/empty.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Empty : MonoBehaviour {
+public class empty : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
