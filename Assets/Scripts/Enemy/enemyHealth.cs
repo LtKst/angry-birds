5b9518e3a7831af358cf1ef394eeac88c1a58449
Yaruno292 <Yarunoyo@outlook.com>
2017-11-30 09:55:03 +0100

    ALOT
    block dmg and states, some ui stuff

---
+++ Assets/Scripts/Enemy/enemyHealth.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyHealth : MonoBehaviour {
+
+    int hp;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
