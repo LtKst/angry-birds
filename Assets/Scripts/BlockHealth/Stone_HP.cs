941a6b8ab180e9ebbc5fcdd4256f479fa4d0b963
Yaruno292 <Yarunoyo@outlook.com>
2017-12-05 12:36:26 +0100

    Updated some scripts

--- Assets/Scripts/BlockHealth/Stone_HP.cs
+++ Assets/Scripts/BlockHealth/Stone_HP.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Wood_HP : MonoBehaviour {
+public class Stone_HP : MonoBehaviour {
 
     public int state;
 
@@ -16,34 +16,22 @@ public class Wood_HP : MonoBehaviour {
 
     SpriteRenderer sp;
 
-    // Use this for initialization
-    void Start()
-    {
+	// Use this for initialization
+	void Start () {
         sp = GetComponent<SpriteRenderer>();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Bird" && timer == 0)
+        if(collision.gameObject.tag == "Bird" && timer == 0)
         {
-            state += 3;
-            UI.score += 500;
-        }
-        if (collision.gameObject.tag == "Ground" && timer == 0)
-        {
-            state += 1;
-            UI.score += 200;
-        }
-        if (collision.gameObject.tag == "Block" && timer == 0)
-        {
-            state += 1;
-            UI.score += 100;
+            state += 2;
+            UI.score += 50000;
         }
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update () {
 
         if (timer > 0)
         {
@@ -62,7 +50,7 @@ public class Wood_HP : MonoBehaviour {
                 //little dmg
                 sp.sprite = littledmg;
             }
-            if (state == 2)
+            if(state == 2)
             {
                 //mid dmg
                 sp.sprite = middmg;
