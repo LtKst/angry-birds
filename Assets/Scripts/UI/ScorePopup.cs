382a29205c3a70f84907ff37594c53b840cb45e7
Gijs Schouten <21969@ma-web.nl>
2017-12-14 10:17:37 +0100

    catapult and ui test

---
+++ Assets/Scripts/UI/ScorePopup.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScorePopup : MonoBehaviour {
+    Rect text;
+    private GUIStyle style;
+    public Font abFont;
+
+    private void Start() {
+        text = new Rect(0,0,1000,1000);
+        style = new GUIStyle();
+
+
+    }
+
+    private void OnGUI() {
+        style.font = abFont;
+        GUILayout.Label("", style);
+        GUI.Label(text, "500");
+                 
+    }
+}
