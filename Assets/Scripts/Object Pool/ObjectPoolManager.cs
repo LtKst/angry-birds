7790bd2b093fed114ffe37f105ecead4fb651409
Koen Sparreboom <koensparreboom@gmail.com>
2017-12-14 09:25:48 +0100

    Improvements & now use new art

--- Assets/Scripts/Object Pool/ObjectPoolManager.cs
+++ Assets/Scripts/Object Pool/ObjectPoolManager.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 
 /// <summary>
 /// Made by Koen Sparreboom
-/// Not totally happy with how this turned out, might fix it later
 /// </summary>
 public class ObjectPoolManager : MonoBehaviour {
 
@@ -25,6 +24,11 @@ public class ObjectPoolManager : MonoBehaviour {
         }
     }
 
+    /// <summary>
+    /// Find an object pool and get an GameObject from it
+    /// </summary>
+    /// <param name="name"></param>
+    /// <returns>GameObject</returns>
     public GameObject SpawnPoolObject(string name) {
         for (int i = 0; i < objectPools.Length; i++) {
             if (objectPools[i].poolName == name) {
@@ -37,6 +41,12 @@ public class ObjectPoolManager : MonoBehaviour {
         return null;
     }
 
+    /// <summary>
+    /// Find an object pool and get an GameObject from it and automatically disable it after a set period of time
+    /// </summary>
+    /// <param name="name"></param>
+    /// <param name="autoDisableTime"></param>
+    /// <returns>GameObject</returns>
     public GameObject SpawnPoolObject(string name, float autoDisableTime) {
         for (int i = 0; i < objectPools.Length; i++) {
             if (objectPools[i].poolName == name) {
@@ -53,6 +63,12 @@ public class ObjectPoolManager : MonoBehaviour {
         return null;
     }
 
+    /// <summary>
+    /// Find an object pool and get an GameObject from it and set it's position
+    /// </summary>
+    /// <param name="name"></param>
+    /// <param name="position"></param>
+    /// <returns>GameObject</returns>
     public GameObject SpawnPoolObject(string name, Vector3 position) {
         for (int i = 0; i < objectPools.Length; i++) {
             if (objectPools[i].poolName == name) {
@@ -68,6 +84,13 @@ public class ObjectPoolManager : MonoBehaviour {
         return null;
     }
 
+    /// <summary>
+    /// Find an object pool and get an GameObject from it, set it's position and automatically disable it after a set period of time
+    /// </summary>
+    /// <param name="name"></param>
+    /// <param name="position"></param>
+    /// <param name="autoDisableTime"></param>
+    /// <returns>GameObject</returns>
     public GameObject SpawnPoolObject(string name, Vector3 position, float autoDisableTime) {
         for (int i = 0; i < objectPools.Length; i++) {
             if (objectPools[i].poolName == name) {
@@ -85,6 +108,12 @@ public class ObjectPoolManager : MonoBehaviour {
         return null;
     }
 
+    /// <summary>
+    /// Automatically disable an GameObject after a set period of time
+    /// </summary>
+    /// <param name="gameObject"></param>
+    /// <param name="time"></param>
+    /// <returns></returns>
     private IEnumerator AutoRemove(GameObject gameObject, float time) {
         yield return new WaitForSeconds(time);
 
